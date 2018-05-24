b92232ddc5ee934922774558fe7b8e9360a9c238
云谦 <yunqian@alibaba-inc.com>
2018-05-24 09:57:12 +0800

    feat: support config opts.copyTo, see README.md for more detail

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
 # @ali/umi-plugin-sofa
 
+## 使用
+
+先安装依赖，
+
+```bash
+$ tnpm i @ali/umi-plugin-sofa -D
+```
+
+编辑 `.umirc.js`，
+
+```js
+export default {
+  plugins: ['@ali/umi-plugin-sofa'],
+};
+```
+
+如果要配置 copyTo 参数，可以这样：
+
+```js
+export default {
+  plugins: [
+    ['@ali/umi-plugin-sofa', {
+      copyTo: [
+        '../webroot/view/home/screen/umi.vm',
+        '../webroot/view/merchant/screen/umi.vm',
+      ],
+    }],
+  ],
+};
+```
+
+默认的 copyTo 是 `['../webroot/view/home/screen/umi.vm']`。
+
 ## 参考
 
 * [给 ifccustmng 的 MR](http://gitlab.alipay-inc.com/alipayplus_ph_release/ifccustmng/merge_requests/8)
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+const assert = require('assert');
 const { copySync } = require('fs-extra');
 const { join } = require('path');
 
-module.exports = function (api) {
+module.exports = function (api, opts = {}) {
   const { cwd } = api.service;
+  const copyTo = opts.copyTo || ['../webroot/view/home/screen/umi.vm'];
+  assert(
+    Array.isArray(copyTo),
+    `[umi-plugin-sofa] opts.copyTo must be Array, but got ${copyTo}`,
+  );
+
   api.register('buildSuccess', function () {
-    copySync(
-      join(cwd, 'dist/index.html'),
-      join(cwd, '../webroot/view/home/screen/umi.vm'),
-    );
-    console.log('[umi-plugin-sofa] Copy dist/index.html to ../webroot/view/home/screen/umi.vm');
+    copyTo.forEach((to) => {
+      copySync(
+        join(cwd, 'dist/index.html'),
+        join(cwd, to),
+      );
+      console.log(`[umi-plugin-sofa] Copy dist/index.html to ${to}`);
+    });
     copySync(
       join(cwd, 'dist/static'),
       join(cwd, '../webroot/public/static'),
